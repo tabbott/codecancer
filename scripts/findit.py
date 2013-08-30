0f0e04d15b256f4cebe2bc2692039d17c7b7890a
Mark Burnett <mark.m.burnett@gmail.com>
2013-08-30 10:59:37 -0500

    add preprocess and findit

---
+++ scripts/findit.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import argparse
+import bisect
+import json
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--index-file', default='positions.json',
+            help='index to lookup file positions in')
+
+    parser.add_argument('position', nargs='+', type=int,
+            help='index of S for which to find the filename and line number')
+
+    return parser.parse_args()
+
+
+class Index(object):
+    def __init__(self, filename):
+        blob = json.load(open(filename))
+        self.filenames = blob['filenames']
+        self.file_start_positions = blob['file_start_positions']
+        self.line_start_positions = blob['line_start_positions']
+
+    def find(self, position):
+        file_index = bisect.bisect(self.file_start_positions, position) - 1
+
+        filename = self.filenames[file_index]
+        file_offset = position - self.file_start_positions[file_index]
+
+        line_number = bisect.bisect(self.line_start_positions[file_index],
+                file_offset)
+        return (filename, line_number)
+
+
+def main():
+    args = parse_args()
+
+    index = Index(args.index_file)
+
+    for position in args.position:
+        filename, line_number = index.find(position)
+        print '%d\t%s\t%d' % (position, filename, line_number)
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/preprocess.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+import argparse
+import itertools
+import json
+import pygments.lexers.agile
+import pygments.token
+import re
+import sys
+
+
+LEXER = pygments.lexers.agile.PerlLexer()
+
+
+def _process_tokens(raw_tokens):
+    results = []
+    line_number = 1
+    for file_position, token_type, text in raw_tokens:
+        stripped_value = text.strip()
+        if stripped_value and token_type not in pygments.token.Comment:
+            results.append(Token(line_number, stripped_value, file_position))
+        line_number += text.count('\n')
+    return results
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--concat-file',
+            help='where to output normalized representation')
+    parser.add_argument('--index-file', default='positions.json',
+            help='where to write position index')
+
+    parser.add_argument('--null-separate', action='store_true',
+            help='separate file contents in concat-file using NULL')
+
+    parser.add_argument('file', nargs='*',
+            help='source files to normalize')
+
+    return parser.parse_args()
+
+
+_DROP_WHITESPACE = re.compile(r'\s')
+def filter_text(raw_text):
+    return _DROP_WHITESPACE.sub('', raw_text)
+
+
+def process(raw):
+    current_position = 0
+
+    line_positions = [0]
+    string_components = []
+
+    for file_position, token_type, text in LEXER.get_tokens_unprocessed(raw):
+        filtered_text = filter_text(text)
+
+        token_line_count = text.count('\n')
+        line_positions.extend(itertools.repeat(current_position,
+            token_line_count))
+
+        if token_type not in pygments.token.Comment:
+            if filtered_text:
+                string_components.append(filtered_text)
+                current_position += len(filtered_text)
+
+
+    return line_positions, ''.join(string_components)
+
+
+def main():
+    args = parse_args()
+
+    file_start_positions = []
+    line_start_positions = []
+
+    if args.concat_file:
+        concat = open(args.concat_file, 'w')
+    else:
+        concat = sys.stdout
+
+    current_file_position = 0
+    for filename in args.file:
+        with open(filename) as f:
+            line_positions, string = process(f.read())
+
+        file_start_positions.append(current_file_position)
+        current_file_position += len(string)
+
+        line_start_positions.append(line_positions)
+        concat.write(string)
+
+    with open(args.index_file, 'w') as index:
+        json.dump({
+            'filenames': args.file,
+            'file_start_positions': file_start_positions,
+            'line_start_positions': line_start_positions,
+        }, index)
+
+
+if __name__ == '__main__':
+    main()
