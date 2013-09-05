68a94601cc0fc26700dfa56a90b0184de73c5fa5
Nathan Nutter <iam@nnutter.com>
2013-09-05 15:11:40 -0500

    add rapidxml library

---
+++ vendor/rapidxml.cmake
@@ -0,0 +1,20 @@
+cmake_minimum_required(VERSION 2.8)
+
+# Build/configure rapidxml
+
+include(ExternalProject)
+set_directory_properties(PROPERTIES
+    EP_PREFIX ${CMAKE_BINARY_DIR}/vendor)
+
+set(RAPIDXML_ROOT ${CMAKE_BINARY_DIR}/vendor/rapidxml)
+ExternalProject_Add(
+    rapidxml-1.13
+    URL ${CMAKE_CURRENT_SOURCE_DIR}/vendor/rapidxml-1.13.tar.gz
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    SOURCE_DIR "${RAPIDXML_ROOT}"
+)
+
+include_directories(${RAPIDXML_ROOT})
+message("RAPIDXML_ROOT set to ${RAPIDXML_ROOT}")
