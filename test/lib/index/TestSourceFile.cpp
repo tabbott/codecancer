90bfac826ab7329bf995f3790283480b05c04ae8
Nathan Nutter <iam@nnutter.com>
2013-09-05 15:45:40 -0500

    pull sources into SetUp

--- test/lib/index/TestSourceFile.cpp
+++ test/lib/index/TestSourceFile.cpp
@@ -13,14 +13,15 @@ public:
             << " \"file_sizes\": [10, 10, 10],\n"
             << " \"line_start_positions\": [[0, 5], [0, 6], [0, 7]]\n"
             << "}";
+        sources = SourceFile::fromJson(jsonIn);
     }
 
 protected:
     std::stringstream jsonIn;
+    std::vector<SourceFile> sources;
 };
 
 TEST_F(TestSourceFile, fromJson) {
-    auto sources = SourceFile::fromJson(jsonIn);
     std::vector<SourceFile> expected{
         {"a",  0,  9, {0, 5}},
         {"b", 10, 19, {0, 6}},
